415578b0d9c510d626d3feada591daf08f877a23
marco.perez <marco.perez@lutech.it>
2025-02-21 08:26:26 +0100

    rimozione componenti che non servono

--- showroom/projects/mp-toolkit/src/lib/mp-toolkit.module.ts
+++ showroom/projects/mp-toolkit/src/lib/mp-toolkit.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MpToolkitComponent } from './mp-toolkit.component';
 
 
 
 @NgModule({
   declarations: [
-    MpToolkitComponent
   ],
   imports: [
   ],
   exports: [
-    MpToolkitComponent
   ]
 })
 export class MpToolkitModule { }
--- showroom/projects/mp-toolkit/src/public-api.ts
+++ showroom/projects/mp-toolkit/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of mp-toolkit
  */
 
-export * from './lib/mp-toolkit.service';
-export * from './lib/mp-toolkit.component';
 export * from './lib/mp-toolkit.module';
