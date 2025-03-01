79368c679c210ce88601dd3dc77aca805cf6fcfb
marco.perez <marco.perez@lutech.it>
2025-03-01 08:47:07 +0100

    applicazione theme Aura e integrazione card in showroom

--- showroom/projects/mp-toolkit/src/lib/mp-toolkit.module.ts
+++ showroom/projects/mp-toolkit/src/lib/mp-toolkit.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { CardComponent } from './components/card/card.component';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,14 @@ import { MenubarModule } from 'primeng/menubar';
   imports: [
     CardModule,
     MenubarModule
+  ],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
   ],
   exports: [
     CardComponent,
